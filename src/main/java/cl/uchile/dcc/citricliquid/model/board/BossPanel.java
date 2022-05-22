d1e6c9c2012df9a7eac172e78f046409a87391b0
Gonzalo Sobarzo <gonzalo.sobarzoa@gmail.com>
2022-05-22 18:45:57 -0400

    Added something to Encounter Panel (not definitive)

--- src/main/java/cl/uchile/dcc/citricliquid/model/board/BossPanel.java
+++ src/main/java/cl/uchile/dcc/citricliquid/model/board/BossPanel.java
@@ -1,7 +1,12 @@
 package cl.uchile.dcc.citricliquid.model.board;
 
 import cl.uchile.dcc.citricliquid.model.Character.Player;
+import cl.uchile.dcc.citricliquid.model.Character.WildUnit;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class BossPanel extends AbstractPanel{
     /**
@@ -19,6 +24,5 @@ public class BossPanel extends AbstractPanel{
      */
     @Override
     public void activatedBy(@NotNull Player player) {
-
     }
 }
--- src/main/java/cl/uchile/dcc/citricliquid/model/board/EncounterPanel.java
+++ src/main/java/cl/uchile/dcc/citricliquid/model/board/EncounterPanel.java
@@ -1,7 +1,12 @@
 package cl.uchile.dcc.citricliquid.model.board;
 
 import cl.uchile.dcc.citricliquid.model.Character.Player;
+import cl.uchile.dcc.citricliquid.model.Character.WildUnit;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class EncounterPanel extends AbstractPanel{
     /**
@@ -14,10 +19,19 @@ public class EncounterPanel extends AbstractPanel{
     }
 
     /**
-     * creates a random wild unit
+     * creates a random wild unit and starts a battle
      * @param player
      */
     @Override
     public void activatedBy(@NotNull Player player) {
+        Random random = new Random();
+        int mini_roll = random.nextInt(3)+1;
+        List<WildUnit> possibleEnemies = new ArrayList<>();
+        possibleEnemies.add(new WildUnit("Chicken",3,-1,-1,1));
+        possibleEnemies.add(new WildUnit("Robo Ball",3,-1,1,-1));
+        possibleEnemies.add(new WildUnit("Seagull",3,1,-1,-1));
+        WildUnit enemy = possibleEnemies.get(mini_roll);
+        //player.battle(enemy); battle function still not implemented, maybe next time have an independent Wild Unit battle function same with the Boss Unit
+
     }
 }
