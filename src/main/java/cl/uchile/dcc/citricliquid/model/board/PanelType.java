d48c4063324c8bbf9a37dea91f41d03f09a3da60
Gonzalo Sobarzo <gonzalo.sobarzoa@gmail.com>
2022-05-28 00:15:50 -0400

    Tarea 1

--- src/main/java/cl/uchile/dcc/citricliquid/model/board/PanelType.java
+++ src/main/java/cl/uchile/dcc/citricliquid/model/board/PanelType.java
@@ -3,7 +3,7 @@ package cl.uchile.dcc.citricliquid.model.board;
 /**
  * Enum for the types of panels of the game.
  *
- * @author <a href="mailto:gonzalo.sobarzo@ug.uchile.cl">Gonzalo Sobarzo A.</a>.
+ * @author <a href="mailto:gonzalo.sobarzo@ug.uchile.cl">Gonzalo Sobarzo A.
  * @version 1.1.222804
  * @since 1.0
  */
